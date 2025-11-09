cfe460efe7949ce877c2ce405b494254b7f286b7
Ivan <ivanvolodin960@gmail.com>
2025-11-09 11:15:15 +0300

    TB-1: add starter kit

---
+++ src/components/sidebar/sidebar-header/SidebarHeader.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { PanelRightOpen } from '@lucide/svelte'
+</script>
+
+<header>
+  <img src="/avatar.png" alt="avatar" />
+  <span>Jessy Barret</span>
+
+  <PanelRightOpen />
+</header>
+
+<style>
+
+</style>
+
